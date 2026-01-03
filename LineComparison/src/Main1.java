85ca5dca315be57d2cae76e18f8474a4a14460a1
DhanasaiBontu <bdhanasaichowdary@gmail.com>
2026-01-03 23:14:55 +0530

    UC2: Check equality of two lines using equals method

--- LineComparison/src/Main1.java
+++ LineComparison/src/Main1.java
@@ -1,10 +1,24 @@
 public class Main1 {
     public static void main(String[] args) {
 
-        LineComparison line = new LineComparison(2, 3, 5, 7);
+        System.out.println("Welcome to Line Comparison Program - UC2");
 
-        double length = line.calculateLength();
+        // Line 1
+        LineComparison line1 = new LineComparison(0, 0, 4, 4);
 
-        System.out.println("Length of the line: " + length);
+        // Line 2
+        LineComparison line2 = new LineComparison(1, 1, 5, 5);
+
+        Double length1 = line1.calculateLength();
+        Double length2 = line2.calculateLength();
+
+        System.out.println("Length of Line 1: " + length1);
+        System.out.println("Length of Line 2: " + length2);
+
+        if (line1.isEqual(line2)) {
+            System.out.println("Both lines are Equal");
+        } else {
+            System.out.println("Lines are Not Equal");
+        }
     }
 }
