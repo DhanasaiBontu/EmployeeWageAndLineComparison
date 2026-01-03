5134387367a800cc08946ae2093a0eb5e937e169
DhanasaiBontu <bdhanasaichowdary@gmail.com>
2026-01-03 22:53:18 +0530

    UC4: Solve employee wage using switch case

--- EmployeeWageComputation/src/EmployeeWageComputation.java
+++ EmployeeWageComputation/src/EmployeeWageComputation.java
@@ -29,4 +29,28 @@ public class EmployeeWageComputation {
         System.out.println("Part Time Employee Wage: " + partTimeWage);
     }
 
+    public void calculateWageUsingSwitch() {
+        int wagePerHour = 20;
+        int fullDayHour = 8;
+        int partTimeHour = 8;
+
+        int empType = new Random().nextInt(3); // 0-Absent, 1-FullTime, 2-PartTime
+        int empWage = 0;
+
+        switch (empType) {
+            case 1:
+                empWage = wagePerHour * fullDayHour;
+                System.out.println("Full Time Wage: " + empWage);
+                break;
+
+            case 2:
+                empWage = wagePerHour * partTimeHour;
+                System.out.println("Part Time Wage: " + empWage);
+                break;
+
+            default:
+                System.out.println("Employee Absent, Wage: 0");
+        }
+    }
+
 }
